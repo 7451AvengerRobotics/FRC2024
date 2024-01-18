ffaae90b17f5586ee7b81d92751ac9a19c0ccb99
rdesai14 <desairishi17@gmail.com>
2024-01-18 16:40:12 -0500

    Adding the Intake

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import frc.robot.Constants;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+    private final CANSparkMax intake;
+
+    public Intake(){
+        super();
+        intake = new CANSparkMax(Constants.intakeMotorCAN, MotorType.kBrushless);
+
+    }
+
+    public void runMotor(double power){
+        intake.set(power);
+    }
+    
+    
+}
