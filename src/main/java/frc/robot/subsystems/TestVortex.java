c736d020c1ebd1141e1fe1f4575b5720920b4731
rdesai14 <desairishi17@gmail.com>
2024-01-28 12:00:29 -0500

    Adding PID loop paired with limelight

--- src/main/java/frc/robot/subsystems/TestVortex.java
+++ src/main/java/frc/robot/subsystems/TestVortex.java
@@ -39,6 +39,13 @@ public class TestVortex extends SubsystemBase{
     SmartDashboard.putBoolean("Limit Switch State", this.detected());
    }
    
+   public void setCount(double counts) {
+      if (counts == 0) {
+        m_pidController.setReference(0, CANSparkFlex.ControlType.kPosition);
+     } else {
+     m_pidController.setReference(counts , CANSparkFlex.ControlType.kPosition);
+   }
+  }
 
    
    // public void setPowerToTestVortex2(double power){
